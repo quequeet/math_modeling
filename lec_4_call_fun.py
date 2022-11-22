4b3c9cb217220a8b6af7a8725e75b4edd3fe3445
quequeet <114501057+quequeet@users.noreply.github.com>
2022-11-22 16:32:24 +0000

    лекция 4

---
+++ lec_4_call_fun.py
@@ -0,0 +1,12 @@
+from lec_4_create_fun import mult_func
+from lec_4_create_fun import print_func
+
+tmp = mult_func(4)
+
+print(mult_func(10))
+
+print(mult_func('Good'))
+
+print_func('Hello')
+
+print_func(mult_func('50'))
---
+++ lec_4_create_fun.py
@@ -0,0 +1,11 @@
+def name_func(arg1, arg2, ..., argN ):
+return 
+
+
+def mult_func(a):
+    x = 3 * a 
+    return x 
+
+def print_func(a):
+    print(f) мой принт с блекджеком и {a}
+    
