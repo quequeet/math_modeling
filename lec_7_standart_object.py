3f2963255572fed24056dc236a5da7151cff77f9
quequeet <114501057+quequeet@users.noreply.github.com>
2023-01-12 17:40:25 +0000

    закомиться

--- lec_7_standart_object.py
+++ lec_7_standart_object.py
@@ -7,7 +7,7 @@ ball, = plt.plot([], [], 'o', color='r', label='Ball')
  
 def circle_move(R, angle_vel, time):
     alpha = angle_vel * (np.pi/180) * time
-    x = R*np.cos(alpha)
+    x = 2*R*np.cos(alpha)
     y = R*np.sin(alpha)
     return x, y
 	
